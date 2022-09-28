98ba7fb2737f1c3e41e3e8327383a8193a4b95f8
Florian Latapie <florian.latapie@etu.unice.fr>
2022-09-28 16:20:02 +0200

    Transformation des objets en Interfaces

--- Client_VOD/src/interfaces/IVODService.java
+++ Client_VOD/src/interfaces/IVODService.java
@@ -1,11 +1,8 @@
 package interfaces;
 
-import objects.Bill;
-import objects.MovieDesc;
-
 import java.util.List;
 
 public interface IVODService extends java.rmi.Remote {
-    List<MovieDesc> viewCatalog();
-    Bill playmovie(String isbn, IClientBox box);
+    List<IMovieDesc> viewCatalog();
+    IBill playmovie(String isbn, IClientBox box);
 }
