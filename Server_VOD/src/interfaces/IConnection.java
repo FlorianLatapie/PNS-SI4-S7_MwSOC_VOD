041bfe365b61bfd0b2ffe9b3c28aebc39c957033
Florian Latapie <florian.latapie@etu.unice.fr>
2022-09-28 16:41:04 +0200

    code cleaning 2

--- Server_VOD/src/interfaces/IConnection.java
+++ Server_VOD/src/interfaces/IConnection.java
@@ -6,7 +6,8 @@ import exceptions.SignInFailed;
 import java.rmi.RemoteException;
 
 public interface IConnection extends java.rmi.Remote {
-    boolean signIn (String mail, String pwd) throws SignInFailed, RemoteException;
+    boolean signIn(String mail, String pwd) throws SignInFailed, RemoteException;
+
     IVODService login(String mail, String pwd) throws InvalidCredentialsException, RemoteException;
 }
 
