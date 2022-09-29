0f3d2fad25ba0bc677fcc57e44a285b0e5e2e728
Florian Latapie <florian.latapie@etu.unice.fr>
2022-09-29 12:07:20 +0200

    meilleure version de l'exception sign up failed

--- Server_VOD/src/exceptions/SignUpFailed.java
+++ Server_VOD/src/exceptions/SignUpFailed.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class SignUpFailed extends Exception {
     public SignUpFailed(String username) {
-        super("SignUpFailed: " + username + " already exists");
+        super("SignUpFailed: User '" + username + "' already exists");
     }
 }
