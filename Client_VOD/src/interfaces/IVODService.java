09b2dd2e8491aa33727060c56dc3763ef2d03fc0
Florian Latapie <florian.latapie@etu.unice.fr>
2022-09-30 08:09:34 +0200

    Amélioration main client (ajout de commentaires)

--- Client_VOD/src/interfaces/IVODService.java
+++ Client_VOD/src/interfaces/IVODService.java
@@ -1,7 +1,6 @@
 package interfaces;
 
 import java.rmi.RemoteException;
-import java.util.List;
 
 public interface IVODService extends java.rmi.Remote {
     String viewCatalog() throws RemoteException;
--- Client_VOD/src/main/Main.java
+++ Client_VOD/src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import exceptions.SignUpFailed;
 import interfaces.IConnection;
 
 import java.rmi.registry.LocateRegistry;
@@ -7,30 +8,37 @@ import java.rmi.registry.Registry;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Client started");
+        System.out.println("Client started !\n");
 
         Registry reg = LocateRegistry.getRegistry("localHost", 2001);
         IConnection c = (IConnection) reg.lookup("Connection");
 
+        System.out.println("Try sign up with existing user ('test', 'test'), should throw an error");
         try {
             c.signUp("test", "test");
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (SignUpFailed e) {
+            System.out.println(e.getMessage() + "\n");
         }
 
+        System.out.println("Try sign up with new user ('Ludo', '123'), should be successful at the first launch during the server lifetime");
         try {
-            c.signUp("Ludo", "123");
+            c.signUp("ludo@mail.com", "123");
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
+        System.out.println();
+
+        System.out.println("Try to log in with not existing user ('jean.daniel@jaimail.com', 'monbeaumotdepasse'), should throw an error");
         try {
-            c.login("GRR", "234");
+            c.login("jean.daniel@jaimail.com", "monbeaumotdepasse");
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage() + "\n");
         }
 
-        var VODService = c.login("Ludo", "123");
+        System.out.println("Try to log in with existing user ('ludo@mail.com', '123'), should be successful");
+        var VODService = c.login("ludo@mail.com", "123");
 
+        System.out.println("Display the catalog :");
         System.out.println(VODService.viewCatalog());
     }
 }
